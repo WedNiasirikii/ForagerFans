e06cea2e289971e1408653c1575300ae29bd12dc
WedNiasirikii <cm7384174@gmail.com>
2023-05-02 16:10:05 -0300

    1.2

--- forager.js
+++ forager.js
@@ -1,28 +1,15 @@
+const btnLanguage = document.getElementById('btn-language')
+btnLanguage.addEventListener('click', () => {
+    let currentUrl = window.location.href;
 
-document.addEventListener('DOMContentLoaded', () => {
-    const btnLanguage = document.querySelector('#btn-language');
-    btnLanguage.addEventListener('click', cambiarIdioma);
+    // Si la URL actual contiene "index_en", cambia a la versión en español
+    if (currentUrl.includes('index-es.html')) {
+        window.location.href = 'index-en.html';
+    }
+    // Si la URL actual contiene "index_es", cambia a la versión en inglés
+    else if (currentUrl.includes('index-en.html')) {
+        window.location.href = 'index-es.html';
+    }
 });
 
-// Definir la función cambiarIdioma
-function cambiarIdioma() {
-    // Obtener el botón de cambiar idioma
-    const btnLanguage = document.getElementById('btn-language');
-
-    // Agregar un evento de clic al botón
-    btnLanguage.addEventListener('click', () => {
-        // Obtener la URL actual de la página
-        const currentUrl = window.location.href;
-
-        // Si la URL actual contiene "index_en", cambia a la versión en español
-        if (currentUrl.includes('index_ingles.html')) {
-            window.location.href = 'index.html';
-        }
-        // Si la URL actual contiene "index_es", cambia a la versión en inglés
-        else if (currentUrl.includes('index')) {
-            window.location.href = 'index_ingles.html';
-        }
-    });
-
-}
 
